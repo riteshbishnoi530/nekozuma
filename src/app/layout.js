ba3d1092095445a9b936053ed83f951287a7bfb6
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-03-04 12:31:56 +0530

    change

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,17 @@ import "./globals.css";
 export const metadata = {
   title: "Nekozuma",
   description: "The collection  of 5555 NFTs on the Solana blockchain. The collection serves to be a rising force against the anime meta of collections with undoxxed teams and is therefore backed by the team at Nubbies NFT & Karmic Labs.",
+  openGraph: {
+    description: "The collection  of 5555 NFTs on the Solana blockchain. The collection serves to be a rising force against the anime meta of collections with undoxxed teams and is therefore backed by the team at Nubbies NFT & Karmic Labs.",
+    images: [
+      {
+        url: "/meta-image.png",
+        width: 800,
+        height: 600,
+        alt: "meta-img",
+      }
+    ]
+  }
 };
 
 export default function RootLayout({ children }) {
