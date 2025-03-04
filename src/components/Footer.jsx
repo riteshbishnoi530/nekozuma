423e49999bcd54f0a5b1ca7aded8cf59dd86f7ff
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-03-04 16:39:30 +0530

    add link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className='footer bg-off-blue position-relative'>
             <img className='position-absolute left-layer' src="/assets/images/footer-left-layer.webp" alt="footer left layer" />
             <img className='position-absolute right-layer' src="/assets/images/footer-right-layer.webp" alt="footer right layer" />
-            <a className='text-base custom-blue d-flex justify-content-center text-white text-center font-franklin footer-heading mb-4' href="/">NEKOZUMA</a>
+            <Link className='text-base custom-blue d-flex justify-content-center text-white text-center font-franklin footer-heading mb-4' href="/">NEKOZUMA</Link>
             <div className='d-flex justify-content-center gap-4'>
                 <Link target='_blank' href="https://x.com/?lang=en" ><img className='icons' src="/assets/images/twitter.webp" alt="twitter" /></Link>
                 <Link target='_blank' href="https://discord.com/" ><img className='icons' src="/assets/images/discord.webp" alt="discord" /></Link>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='custom-container mx-auto d-flex justify-content-sm-end pt-sm-4 pt-lg-3 pt-5'>
-            <a href="/" className='position-absolute top-0 start-0'>
+            <Link href="/" className='position-absolute top-0 start-0'>
                 <img className='logo' src="/assets/images/logo.webp" alt="logo" />
-            </a>
+            </Link>
             <div className='d-flex align-items-center header-gap'>
                 <ul className='d-flex header-gap p-0'>
                     {HEADER_LIST.map((obj, i) => (
-                        <li key={i}><a href={obj.link} className='text-white header-link'>{obj.title}</a>
+                        <li key={i}><Link href={obj.link} className='text-white header-link'>{obj.title}</Link>
                         </li>
                     ))}
                 </ul>
